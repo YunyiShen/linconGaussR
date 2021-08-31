00c071a203ac75eab22f1085aa6376a4cb2e037e
YunyiShen <yshen99@wisc.edu>
2021-08-31 17:48:57 -0500

    init commit, some header

---
+++ README.md
@@ -0,0 +1,8 @@
+# `linconGaussR`: partial R implementation of `LinConGauss`
+###  _samples of Gaussians under linear domain constraints_
+
+**Still under development, i.e. not finished**
+
+The original Python implementation can be found [here](https://github.com/alpiges/LinConGauss). The reference of `LinConGauss` is this [paper](https://arxiv.org/abs/1910.09328). It's very likely this repo will only implement the sampling methods in the near future.
+
+
---
+++ src/loop.h
@@ -0,0 +1,36 @@
+// base class for loop
+
+#ifndef LOOP_H
+#define LOOP_H
+
+#include "linear_constraints.h"
+
+class Loop{
+    public:
+        LinearConstraints lincon;
+        int n_skip;
+    Loop(LinearConstraints linear_con, int nskip){
+        lincon = linear_con;
+        n_skip = nskip;
+    }
+    void run(){
+        return;
+    }
+}
+
+
+class SamplingLoop: public Loop{
+    public: 
+        int n_iterations;
+        SamplingLoop(int n_iter, LinearConstraints linear_con, int nskip){
+            n_iterations = n_iter;
+            lincon = linear_con;
+            n_skip = nskip;
+        }
+        arma::mat compute_next_point(arma::mat x0){
+            return x0;
+        }
+}
+
+
+#endif
